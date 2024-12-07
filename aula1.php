83898a8dedc550202ca5c34bc19584345d308daa
fjportella <fjportella@edu.uniube.br>
2024-12-06 22:05:20 -0300

    Primeiro Commit

---
+++ aula1.php
@@ -0,0 +1,18 @@
+<?php
+$nome = "Fernando";
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Aula 1</title>
+  </head>
+  <body>
+    <h1>Olá Mundo</h1>
+    <h2><?="Eu sou {$nome}"?></h2>
+  </body>
+</html>
